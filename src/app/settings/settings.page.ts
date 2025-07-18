dec116b5d4a01144acc0b54492b4072cde0fd676
victor <contactovictore@gmail.com>
2025-07-18 11:51:09 -0500

    feat: add project initialization

---
+++ src/app/settings/settings.page.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import { IonicModule } from "@ionic/angular";
+
+@Component({
+  selector: 'app-settings',
+  templateUrl: './settings.page.html',
+  styleUrls: ['./settings.page.scss'],
+  imports: [IonicModule],
+})
+export class SettingsPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
